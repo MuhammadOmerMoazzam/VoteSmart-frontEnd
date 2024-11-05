6ad90bfd3ab3392a6fed6c94433d1d515a354249
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-05 23:42:56 +0500

    fix bugs

--- script.js
+++ script.js
@@ -4,23 +4,17 @@ let currentIndex = 0;
     const totalImages = images.length;
 
     function showNextImage() {
-        // Remove 'active' class from the current image and overlay group
         images[currentIndex].classList.remove('active');
         overlayGroups[currentIndex].classList.remove('active');
         
-        // Update the index to the next image
+        
         currentIndex = (currentIndex + 1) % totalImages;
         
-        // Add 'active' class to the new image and overlay group
         images[currentIndex].classList.add('active');
         overlayGroups[currentIndex].classList.add('active');
     }
-
-    // Set the first image and overlay group as active initially
     images[0].classList.add('active');
     overlayGroups[0].classList.add('active');
-
-    // Change image and overlay every 3 seconds
     setInterval(showNextImage, 3000);
 
 
